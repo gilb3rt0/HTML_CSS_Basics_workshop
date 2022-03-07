444e0ab1cbaa96d56539368ea861d69b48840bfa
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-03-07 10:07:22 +0100

    server issue fixed

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+var path = require("path");
 // const path = require('./style.css')
 
-const server = http.createServer(function(req, res){
-   res.writeHead(200, { 'Content-Type': 'text/html' })
-   fs.readFile('index.html', function(error, data){
-       if (error) {
-       res.writeHead(404)
-       res.write('Error: File Not Found')
-   } else {
-        res.write(data)
-   }
-   res.write('Hello Node') 
-   res.end()
-    })
-})
+const server = http.createServer(function (req, res) {
+	res.writeHead(200, { "Content-Type": "text/html" });
 
-server.listen(port, function(error){
-    if (error) {
-        console.log('Something went wrong', error)
-    } else {
-        console.log('Server is listening on port ' + port)
-    }
-})
+	if (req.url === "/") {
+		fs.readFile("index.html", function (error, data) {
+			if (error) {
+				res.writeHead(404);
+				res.write("Error: File Not Found");
+			} else {
+				res.write(data);
+			}
+			res.write("Hello Node");
+			res.end();
+		});
+	} else if (req.url.match(".css")) {
+		var cssPath = path.join(__dirname, "", req.url);
+		var fileStream = fs.createReadStream(cssPath, "UTF-8");
+		res.writeHead(200, { "Content-Type": "text/css" });
+		fileStream.pipe(res);
+	}
+});
+
+server.listen(port, function (error) {
+	if (error) {
+		console.log("Something went wrong", error);
+	} else {
+		console.log("Server is listening on port " + port);
+	}
+});
